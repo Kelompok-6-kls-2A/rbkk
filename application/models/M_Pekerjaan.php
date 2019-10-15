a1ca711d23a7be374b05a4d3ab3ca693cb864299
adindajelfani <jelf.aj@gmail.com>
2019-10-15 17:15:48 +0700

    index

--- application/models/M_Pekerjaan.php
+++ application/models/M_Pekerjaan.php
@@ -5,7 +5,7 @@ class M_pekerjaan extends CI_Model
 {
 	private $_table = "tb_pekerjaan";
 
-	public function getAll($id = null)
+	public function getAll($id = null)//untuk panggil data
 	{
 		# code...
 		if ($id === null) {
@@ -21,7 +21,7 @@ class M_pekerjaan extends CI_Model
 				->FROM($this->_table)
 				->JOIN('tb_user', 'tb_user.id_user = tb_pekerjaan.id_user')
 				->WHERE('id_pekerjaan', $id);
-			$query = $this->db->get()->result_array();
+			$query = $this->db->get()->result_array(); //untuk ambil data. dibrowser aja rbkk/api/user''
 			return $query;
 		}
 	}
