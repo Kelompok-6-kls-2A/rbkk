050c7ed20ef45ceb1d54914f84391c0c4741d8b8
qodirtok <qodirtok@gmail.com>
2019-10-15 16:50:59 +0700

    Update Sidebar.php

--- application/views/Template/Sidebar.php
+++ application/views/Template/Sidebar.php
@@ -29,9 +29,14 @@
 							<h3>General</h3>
 							<ul class="nav side-menu">
 								<li><a href="<?php echo base_url() ?>dashboard"><i class="fa fa-home"></i> Dashboard</a></li>
+								<li><a><i class="fa fa-edit"></i> Master<span class="fa fa-chevron-down"></span></a>
+									<ul class="nav child_menu">
+										<li><a href="<?php echo base_url() ?>user">Pekerjaan</a></li>
+									</ul>
+								</li>
 								<li><a><i class="fa fa-user"></i> Akun<span class="fa fa-chevron-down"></span></a>
 									<ul class="nav child_menu">
-										<li><a href="<?php echo base_url()?>user">Data Akun</a></li>
+										<li><a href="<?php echo base_url() ?>user">Data Akun</a></li>
 									</ul>
 								</li>
 							</ul>
