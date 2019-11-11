a422830ae2ca977f9e43fc7dd404d78ef318f9ec
qodirtok <qodirtok@gmail.com>
2019-11-11 23:26:36 +0700

    a

---
+++ application/models/api/M_API.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_API extends CI_Model
+{
+	public function getAll()
+	{
+		# code...
+	}
+}
+
+/* End of file M_API.php */
