6a553ae178e6fcd6a9b246d26bc3f96bf42bed59
qodirtok <qodirtok@gmail.com>
2019-11-12 18:48:27 +0700

    Update My_Model.php

---
+++ application/models/client/My_Model.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class My_Model extends CI_Model
+{
+	public function get_all()
+	{
+		# code...
+	}
+}
+
+/* End of file My_Model.php */
